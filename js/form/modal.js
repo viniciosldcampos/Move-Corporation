bf90c9a565b1903b91c94ed5e37adbdcbda3ea38
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-04-16 12:35:23 -0300

    Adicionar uma tela modal para o campo de motoristas

---
+++ js/form/modal.js
@@ -0,0 +1,18 @@
+/*====================================*/
+/* Modal para Adicionar informações do motorista interno */
+    function abrirModalInternalDriver() {
+        document.getElementById('modalInternalDriver').style.display = 'flex';
+    }
+
+    function fecharModalInternalDriver() {
+        document.getElementById('modalInternalDriver').style.display = 'none';
+    }
+
+/* Modal para Adicionar informações do motorista externo */
+    function abrirModalExternalDriver() {
+        document.getElementById('modalExternalDriver').style.display = 'flex';
+    }
+
+    function fecharModalExternalDriver() {
+        document.getElementById('modalExternalDriver').style.display = 'none';
+    }
