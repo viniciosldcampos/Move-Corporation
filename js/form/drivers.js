995768b6c05e087111ec87d04d532b01c4db11b4
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-04-17 15:49:40 -0300

    Adicionando lista de motoristas internos

---
+++ js/form/drivers.js
@@ -0,0 +1,9 @@
+// Lista de motoristas cadastrados
+export const drivers = [
+    {name: "João Silva"},
+    {name: "Carlos Oliveira"},
+    {name:"Pedro Santos"},
+    {name:"Lucas Ferreira"},
+    {name: "Marcos Souza"},
+    {name: "Luciana Alves"}
+];
