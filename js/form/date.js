a0ee9c254bd9bb3bc264af8ae184730ab3f0d135
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-04-17 22:29:13 -0300

    Exportando essa função date para o formulário form.js

--- js/form/date.js
+++ js/form/date.js
@@ -1,5 +1,5 @@
 /* Impede selecionar datas passadas no campo de input de data */
-document.addEventListener("DOMContentLoaded", () => {
+export const date = document.addEventListener("DOMContentLoaded", () => {
     const hoje = new Date().toISOString().split("T")[0];
 
     document.querySelectorAll(".date-input").forEach(input => {
