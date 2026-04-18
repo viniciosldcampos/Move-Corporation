8c4ca406e4b50f7d0c9694dac2b9da102129296c
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-04-17 22:46:21 -0300

    Desenvolvendo o modal de motorista interno e exportando globalmente as funções de abrir e fechar os modais

--- js/form/modal.js
+++ js/form/modal.js
@@ -1,18 +1,66 @@
 /*====================================*/
+/* Importar lista de motoristas */
+import { drivers } from "./drivers.js";
+
+
+/*====================================*/
 /* Modal para Adicionar informações do motorista interno */
-    function abrirModalInternalDriver() {
-        document.getElementById('modalInternalDriver').style.display = 'flex';
-    }
 
-    function fecharModalInternalDriver() {
-        document.getElementById('modalInternalDriver').style.display = 'none';
-    }
+export function openModalInternalDriver() {
+    document.getElementById('modalInternalDriver').style.display = 'flex';
+    LoadingDrivers();
+}
 
+export function closeModalInternalDriver() {
+    document.getElementById('modalInternalDriver').style.display = 'none';
+}
+
+
+/*====================================*/
+/* Função para preencher o select */
+
+function LoadingDrivers() {
+    const select = document.getElementById('selectDrivers');
+
+    select.innerHTML = '<option value="">Selecione um motorista</option>';
+
+    drivers.forEach(({ name }) => {
+        const option = document.createElement('option');
+
+        option.value = name;
+        option.textContent = name;
+
+        select.appendChild(option);
+    });
+}
+
+
+/*====================================*/
 /* Modal para Adicionar informações do motorista externo */
-    function abrirModalExternalDriver() {
-        document.getElementById('modalExternalDriver').style.display = 'flex';
-    }
 
-    function fecharModalExternalDriver() {
-        document.getElementById('modalExternalDriver').style.display = 'none';
-    }
+export function openModalExternalDriver() {
+    document.getElementById('modalExternalDriver').style.display = 'flex';
+}
+
+export function closeModalExternalDriver() {
+    document.getElementById('modalExternalDriver').style.display = 'none';
+}
+
+
+/*====================================*/
+/* DOM carregado */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    /* Quando selecionar motorista */
+    document.getElementById('selectDrivers')
+    ?.addEventListener("change", function () {
+        document.getElementById('selectedDriver').value = this.value;
+    });
+});
+
+/* Tornar funções globais */
+window.openModalInternalDriver = openModalInternalDriver;
+window.closeModalInternalDriver = closeModalInternalDriver;
+window.openModalExternalDriver = openModalExternalDriver;
+window.closeModalExternalDriver = closeModalExternalDriver;
