65e40a67f9cc8c932736345e2c62c4c26ab8c928
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-05-06 23:45:59 -0300

    Instalação do Prisma ORM para a conexão do banco de dados com a minha aplicação

--- js/form/drivers.js
+++
@@ -1,9 +0,0 @@
-// Lista de motoristas cadastrados
-export const drivers = [
-    {name: "João Silva"},
-    {name: "Carlos Oliveira"},
-    {name:"Pedro Santos"},
-    {name:"Lucas Ferreira"},
-    {name: "Marcos Souza"},
-    {name: "Luciana Alves"},
-];
