186736819447fc90f38c8c1d97703e6c3f7a2f72
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-05-08 16:01:29 -0300

    Adiciona suporte a caminhos em ES Modules fileURLToPath e dirname, configuração de arquivos estáticos e nova rota de departamentos

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 
 dotenv.config()
 
 const app = express()
+
+// Descobre o caminho do arquivo atual
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// Serve os arquivos estáticos do frontend
+app.use(express.static(join(__dirname, './')))
+
 app.use(express.json())
 
 // Permite o frontend acessar o backend (CORS)
@@ -13,9 +23,12 @@ app.use((req, res, next) => {
   next()
 })
 
+
 // Rotas
 import solicitacoesRoutes from './routes/solicitacoes.js'
+import departamentosRoutes from './routes/departamentos.js'
 app.use('/api/solicitacoes', solicitacoesRoutes)
+app.use('/api/departamentos', departamentosRoutes)
 
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000')
