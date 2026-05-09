200dd99c3f80a3ee2a0da431ea432a798cfc2358
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-05-08 23:27:13 -0300

    Criação de script inicial de usuários no banco de dados para cadastrar usuários de teste do sistema

---
+++ src/scripts/criarUsuario.js
@@ -0,0 +1,32 @@
+import bcrypt from 'bcrypt'
+import prisma from '../database/prisma.js'
+
+async function criarUsuario() {
+  const senhaHash = await bcrypt.hash('123456', 10)
+
+  // Usuário comum
+  await prisma.usuarios.create({
+    data: {
+      pessoa_id: 1,
+      email: 'usuario@teste.com',
+      senha_hash: 123,
+      perfil: 'usuario'
+    }
+  })
+
+  // Gestor
+  await prisma.usuarios.create({
+    data: {
+      pessoa_id: 1,
+      email: 'gestor@teste.com',
+      senha_hash: 123,
+      perfil: 'gestor'
+    }
+  })
+
+  console.log('Usuários criados com sucesso!')
+}
+
+criarUsuario()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect())
