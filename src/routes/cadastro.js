d6107c8b1420e347e6fc27328a6b6178b4b20169
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-05-11 09:56:05 -0300

    Criação de rotas para funcionários, cargos, veículos, motorista e departamentos

--- src/routes/cadastro.js
+++ src/routes/cadastro.js
@@ -1,30 +1,36 @@
 import { Router } from 'express'
 import {
-  listarDepartamentos,
-  listarFuncionarios,
-  listarMotoristas,
-  listarMotoristasPendentes,
-  listarMotoristasAprovados,
-  listarVeiculos,
-  cadastrarFuncionario,
-  cadastrarDepartamento,
-  cadastrarMotorista,
-  cadastrarVeiculo,
-  aprovarMotorista
+  listarDepartamentos, listarCargos, listarFuncionarios,
+  listarMotoristas, listarMotoristasPendentes, listarMotoristasAprovados,
+  listarVeiculos, cadastrarFuncionarioComUsuario, cadastrarDepartamento,
+  cadastrarMotorista, cadastrarVeiculo, aprovarMotorista,
+  editarFuncionario, desativarFuncionario,
+  editarDepartamento, desativarDepartamento,
+  editarVeiculo, desativarVeiculo,
+  editarMotorista, desativarMotorista
 } from '../controllers/CadastroController.js'
 
 const router = Router()
 
 router.get('/departamentos', listarDepartamentos)
+router.get('/cargos', listarCargos)
 router.get('/funcionarios', listarFuncionarios)
 router.get('/motoristas', listarMotoristas)
 router.get('/motoristas/pendentes', listarMotoristasPendentes)
 router.get('/motoristas/aprovados', listarMotoristasAprovados)
 router.get('/veiculos', listarVeiculos)
-router.post('/funcionario', cadastrarFuncionario)
+router.post('/funcionario', cadastrarFuncionarioComUsuario)
 router.post('/departamento', cadastrarDepartamento)
 router.post('/motorista', cadastrarMotorista)
 router.post('/veiculo', cadastrarVeiculo)
 router.patch('/motorista/:id/aprovar', aprovarMotorista)
+router.patch('/funcionario/:id', editarFuncionario)
+router.patch('/funcionario/:id/desativar', desativarFuncionario)
+router.patch('/departamento/:id', editarDepartamento)
+router.patch('/departamento/:id/desativar', desativarDepartamento)
+router.patch('/veiculo/:id', editarVeiculo)
+router.patch('/veiculo/:id/desativar', desativarVeiculo)
+router.patch('/motorista/:id', editarMotorista)
+router.patch('/motorista/:id/desativar', desativarMotorista)
 
 export default router
